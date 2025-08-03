7c2fee9d8de93352ac12dfb8f6a4ea7c070e2477
Yaroslav Shcherban <yaroslav.shcherban@sisense.com>
2025-08-03 21:27:29 +0300

    modify arrays sort

--- c_arrays_challenge/arrays_sort.c
+++ c_arrays_challenge/arrays_sort.c
@@ -1,38 +1,28 @@
 #include <stdio.h>
 
-int main()
-{    
-    int numbers[5] = {30, 10, 3, 11, 0};
+int main() {
+    int numbers[5] = {34, 3, 23, 0, 2};
+    int size = 5;
 
+    for (int i = 0; i < size; i++) {
+        for (int k = i + 1; k < size; k++) {
+            if (numbers[i] > numbers[k]) {
+                int temp = numbers[k];
 
-    for (int i = 0 ; i < 5; i++) {
-        int recorded_array[5];
-        
-        if(i == 0) {
-            recorded_array[0] = numbers[0]; // current element of original array
-
-            for(int k = 1; k < 5; k++) {
-                if(recorded_array[0] > numbers[k]) {
-                    int length = sizeof(numbers) / sizeof(numbers[0]);
-
-                    recorded_array[length + 1] = recorded_array[0];
-                    
-                    recorded_array[0] = numbers[k];
+                for (int j = k; j > i; j--) {
+                    numbers[j] = numbers[j - 1];
                 }
+                numbers[i] = temp;
+
+                i = -1;
+                break;
             }
-
         }
-
-        for (int v = 0; v < 5; v++) {
-            printf("%d ", recorded_array[v]);
-        }
-
-        printf("\n");
-
-        break;
-
     }
 
+    for (int l = 0; l < size; l++) {
+        printf("%d ", numbers[l]);
+    }
 
     return 0;
 }
